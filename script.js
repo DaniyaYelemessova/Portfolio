d5b86f464de9a500c9d4db38b9ff9e8d31c88b09
Daniya Yelemessova <daniya@collingsmail.com>
2023-10-21 20:19:52 -0400

    Add link to GSAP animation library

---
+++ script.js
@@ -0,0 +1,5 @@
+// Анимация
+gsap.from(".main__h2", {x: 100, duration: 1.5})
+gsap.from(".main__item", {x: -50, duration: 1.5})
+gsap.from(".buttons", {y: -50, duration: 1.5, opacity: 0, delay: 0.5})
+gsap.from(".navbar", {y: -50, duration: 1.5, opacity: 0, delay: 0.7})
