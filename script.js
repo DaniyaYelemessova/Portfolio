fcde43cb826335e169b2fe7e5d27745ae05eda5e
Daniya Yelemessova <daniya@collingsmail.com>
2023-10-21 21:27:00 -0400

    Add animation

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 // Анимация
+
 gsap.from(".main__h2", {x: 100, duration: 1.5})
 gsap.from(".main__item", {x: -50, duration: 1.5})
 gsap.from(".buttons", {y: -50, duration: 1.5, opacity: 0, delay: 0.5})
 gsap.from(".navbar", {y: -50, duration: 1.5, opacity: 0, delay: 0.7})
+
+
+gsap.from(".h2", {
+  scrollTrigger: {
+    trigger: ".h2"
+},
+x: 100,
+duration: 1.5
+});
+
+
